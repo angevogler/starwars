319278d0baae26bb9cedc0d5308b823a06a2f170
Angela Vogler <angelavogler4@gmail.com>
2017-09-14 12:42:38 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+imporimport React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
